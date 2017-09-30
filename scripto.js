d111f045f8d36d8c4c502e6a0d719cbfb243ce44
Falkhan <falkhan.mage@gmail.com>
2017-09-30 20:06:29 +0200

    Finish the basic homepage

---
+++ scripto.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  // Setting variables
+  var today = new Date();
+  var message;
+
+  // Greeting user depending on the hour
+  if (today.getHours() > 0 && today.getHours() < 12){
+      message = "Good morning, user.";
+  } else if (today.getHours() >= 12 && today.getHours() < 17) {
+    message = "Good afternoon, user.";
+  } else if (today.getHours() >= 17){
+    message = "Good evening, user."
+  };
+  message += "</br>";
+
+  // Telling what date is today
+  var calendar = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July",
+                  8: "August", 9: "September", 10:"October", 11:"November", 12:"December"};
+  message += "Today is ";
+  if (today.getDay() < 10) {
+    message += "0";
+  };
+  message += today.getDay() + " " + calendar[today.getMonth()] + ", " + today.getFullYear() + ".</br>";
+  addWelcome(message);
+  // Checking the current BTC->EUR
+  var btc_url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+  $.getJSON(btc_url, function(data){
+      console.log(data);
+      message = "BTC to EUR: " + data["bpi"]["EUR"]["rate"] + "</br>";
+      addWelcome(message);
+  });
+});
+
+// Getting a random integer
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function addWelcome(msg){
+  $("#welcome-message").append(msg);
+}
